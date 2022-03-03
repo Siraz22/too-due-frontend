341aaaccb43ebc8300864de8cad464eb61694594
siraz <siraznaorem07@gmail.com>
2022-03-03 11:14:47 +0530

    Route for Generic and Interviewbit Task

--- src/components/InterviewBitTasks/InterviewbitTaskComponent.js
+++ src/components/InterviewBitTasks/InterviewbitTaskComponent.js
@@ -1,8 +1,34 @@
+import react from 'react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Table } from 'reactstrap'
 
 function InterviewbitTaskComponent() {
   return (
-    <div>InterviewbitTaskComponent</div>
+    <react.Fragment>
+      <div className="table-responsive">
+        <Table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Ques</th>
+              <th>Difficulty</th>
+              <th>Link</th>
+              <th>Notes</th>
+              <th>Actions</th>
+            </tr>
+            <tr>
+              <td>Sample</td>
+              <td>Sample Ques</td>
+              <td>Medium</td>
+              <td><a href="https://interviewbit.com">www.interviewbit.com</a></td>
+              <td>Sample Note</td>
+              <td>Buttons</td>
+            </tr>
+          </thead>
+        </Table>
+      </div>
+    </react.Fragment >
   )
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
 import GenericTaskComponent from './CustomTasks/GenericTaskComponent'
+import InterviewbitTaskComponent from './InterviewBitTasks/InterviewbitTaskComponent'
 import { FaGithub } from 'react-icons/fa'
+import { BrowserRouter as Switch, Link, Route } from 'react-router-dom'
 
 function TaskList() {
 
@@ -10,36 +12,54 @@ function TaskList() {
 
   return (
     < div className="row " >
-      <div className="col-8">
-        <Dropdown isOpen={dropdownOpen} toggle={() => setToggle(!dropdownOpen)}>
-          <DropdownToggle caret color="btn btn-outline-dark">
-            {dropdownValue}
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem header>Select List Type</DropdownItem>
-            <DropdownItem onClick={(e) => setDropdownValue(e.currentTarget.textContent)}>Generic Tasks</DropdownItem>
-            <DropdownItem onClick={(e) => setDropdownValue(e.currentTarget.textContent)}>Interviewbit</DropdownItem>
-            <DropdownItem onClick={(e) => setDropdownValue(e.currentTarget.textContent)}>Investments</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </div>
-      <div className="col-4">
-        <FaGithub fontSize={22} />{' '}
-        <p style={{ marginBottom: "0" }}>Join to contribute and learn?</p>{' '}
+      <Switch>
+        <div className="col-8">
 
-        <a className="text-decoration-none" href="https://github.com/Siraz22/too-due-frontend">Frontend
-        </a>
-        {' '}or{' '}
-        <a className="text-decoration-none" href="https://github.com/Siraz22/too-due-backend">Backend
-        </a>
+          <Dropdown isOpen={dropdownOpen} toggle={() => setToggle(!dropdownOpen)}>
+            <DropdownToggle caret color="btn btn-outline-dark">
+              {dropdownValue}
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Select List Type</DropdownItem>
+              <Link className="text-decoration-none" to="/generic">
+                <DropdownItem onClick={(e) => setDropdownValue(e.currentTarget.textContent)}>Generic Tasks</DropdownItem>
+              </Link>
+              <Link className="text-decoration-none" to="/interviewbit">
+                <DropdownItem onClick={(e) => setDropdownValue(e.currentTarget.textContent)}>Interviewbit</DropdownItem>
+              </Link>
+              <Link className="text-decoration-none" to="/investments">
+                <DropdownItem onClick={(e) => setDropdownValue(e.currentTarget.textContent)}>Investments</DropdownItem>
+              </Link>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+        <div className="col-4">
+          <FaGithub fontSize={22} />{' '}
+          <p style={{ marginBottom: "0" }}>Join to contribute and learn?</p>{' '}
 
-      </div>
+          <a className="text-decoration-none" href="https://github.com/Siraz22/too-due-frontend">Frontend
+          </a>
+          {' '}or{' '}
+          <a className="text-decoration-none" href="https://github.com/Siraz22/too-due-backend">Backend
+          </a>
 
-      <div style={{
-        paddingTop: "30px"
-      }}>
-        <GenericTaskComponent />
-      </div>
+        </div>
+
+        <div style={{
+          paddingTop: "30px"
+        }}>
+          <Route exact path="/">
+            <GenericTaskComponent />
+          </Route>
+          <Route exact path="/generic">
+            <GenericTaskComponent />
+          </Route>
+          <Route exact path="/interviewbit">
+            <InterviewbitTaskComponent />
+          </Route>
+
+        </div>
+      </Switch>
 
     </div >
   )
